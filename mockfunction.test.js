96cfd2550e2555251e3b2b8f8c8baacaf14e630f
Sushil Tiwari <sushilt@YarsaPKRMBP001.local>
2025-11-13 13:37:46 +0545

    basics of .mock property

--- mockfunction.test.js
+++ mockfunction.test.js
@@ -31,5 +31,47 @@ const mockCallback = jest.fn(x=> 42 + x);
 test("forEach mock function", () => {
     forEach([0, 1], mockCallback);
     expect(mockCallback).toHaveBeenCalled();
-    console.log(mockCallback(0, 1))
+    // console.log(mockCallback(0, 1))
 })
+
+// .mock property
+const mymock = jest.fn();
+mymock('apple');
+mymock('banana');
+mymock(12);
+
+test("Mock function call passed", () =>{
+    expect(mymock).toHaveBeenCalledTimes(3)
+    expect(mymock).toHaveBeenCalledWith('apple')
+    expect(mymock).toHaveBeenCalledWith('banana')
+    expect(mymock).toHaveBeenCalledWith(12)
+})
+console.log(mymock.mock.calls);
+
+
+// testing th mock funciton
+
+function userName(){
+    return 'Sushil is my name'
+}
+
+const Name = jest.fn().mockReturnValue('Sushil');
+test("The first Username is Sushil", ()=> {
+    expect(Name()).toMatch('Sushil')
+});
+console.log(Name())
+
+const advice = jest.fn();
+advice('Do not do drugs.');
+advice('Do not drink and drive.')
+advice('Working on yourself is a good habit')
+
+console.log(advice.mock.calls)
+console.log(advice.mock.calls.length)
+
+test("Advice function has a list of 3 advices", ()=> {
+    expect(advice).toHaveBeenCalledTimes(3);
+});
+test("Advice function talks about drugs.", ()=> {
+    expect(advice).toHaveBeenCalledWith('Do not do drugs.');
+});
