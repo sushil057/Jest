7a76394c416686c26ace997ea71cc136272e308e
Sushil Tiwari <sushilt@YarsaPKRMBP001.local>
2026-01-26 15:56:06 +0545

    added expected response checks

--- API_test_jest/get.test.js
+++ API_test_jest/get.test.js
@@ -1,12 +1,24 @@
 const axios = require('axios');
 
+
 describe('Get request fetched the data successfully from the API', ()=>{
     test('Get request provides the list of users', async ()=>{
+        const startTime = Date.now();
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        const endTime = Date.now();
+        const responseTime = endTime - startTime;
         expect(response.status).toBe(200);
-        // expect(response.data.length).toBeGreaterThan(0);
-        expect(response.data[1]).toHaveProperty('userId','id','title','completed');
+        expect(responseTime).toBeLessThan(500);
+        expect(response.data.length).toBeGreaterThan(0);
+        expect(response.data[1]).toHaveProperty('userId');
+        expect(response.data[1]).toHaveProperty('id');
+        expect(response.data[1]).toHaveProperty('title');
+        expect(response.data[1]).toHaveProperty('completed');
+        expect(typeof response.data).toBe('Object');
         console.log(response.data[5]);
+        console.log(responseTime)
 
-    })
+    });
+
+    
 })
--- API_test_jest/post.test.js
+++ API_test_jest/post.test.js
@@ -1,4 +1,5 @@
-const axios = require('axios')
+const axios = require('axios');
+// const { expect } = require('chai');
 
 describe('Post request', () =>{
     test('The post request has been executed successfully', async()=>{
@@ -7,10 +8,15 @@ describe('Post request', () =>{
             title: 'this is a test post request',
             completed: false
         }
-        const response = await axios.post('https://jsonplaceholder.typicode.com/posts')
+        const startTime = Date.now();
+        const response = await axios.post('https://jsonplaceholder.typicode.com/posts');
+        const endTime = Date.now();
+        const responseTime = endTime-startTime;
         expect(response.status).toBe(201);
-        expect(response.data).toHaveProperty('id')
-        console.log(response.data)
+        expect(response.data).toHaveProperty('id');
+        expect(responseTime).toBeLessThan(600);
+        console.log(response.data);
+        console.log(responseTime);
 
     })
 })
