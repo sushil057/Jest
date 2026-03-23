febe147b63f7d372e5e2b02a955db3aa6759318a
Sushil Tiwari <sushilt@YarsaPKRMBP001.local>
2026-03-23 14:38:22 +0545

    updated the versions

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 describe('Server Check', () => {
-    test('API Server is up and returns welcome message', async () => {
+  test('API Server is up and returns welcome message', async () => {
     const startTime = Date.now();
     const response = await axios.get('https://dev-api.yarsaplay.com/api');
     const endTime = Date.now();
@@ -12,18 +12,18 @@ describe('Server Check', () => {
     console.log('Welcome Message:', response.data);
     console.log('Response Time: ', responseTime);
   });
-  describe('Server Health Check', ()=>{
-    test('The server is running well', async ()=>{
-        const startTime = Date.now();
-    const response = await axios.get('https://dev-api.yarsaplay.com/api/health');
-    const endTime = Date.now();
-    const responseTime = endTime - startTime;
+  describe('Server Health Check', () => {
+    test('The server is running well', async () => {
+      const startTime = Date.now();
+      const response = await axios.get('https://dev-api.yarsaplay.com/api/health');
+      const endTime = Date.now();
+      const responseTime = endTime - startTime;
 
-    expect(response.status).toBe(200);
-    expect(response.data.status).toBe('ok');
-    expect(response.data.info.prisma.status).toBe('up');
-    console.log('Server Response:\n', response.data);
-    console.log('Response Time: ', responseTime);
+      expect(response.status).toBe(200);
+      expect(response.data.status).toBe('ok');
+      expect(response.data.info.prisma.status).toBe('up');
+      console.log('Server Response:\n', response.data);
+      console.log('Response Time: ', responseTime);
     })
 
   });
